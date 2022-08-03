900af62134ff20e5d5f0b293563acd109e3b4ef1
Brett Houtz <bcat38@gmail.com>
2022-08-02 22:11:30 -0400

    Add index back

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import * as header from './header';
+import * as log from './log';
+import * as message from './message';
+export { header, log, message };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as header from './header';
+import * as log from './log';
+import * as message from './message';
+
+export { header, log, message };
